71c7fb033d268427dea69488e2b70c5b9850a0c0
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-05-11 19:18:56 -0400

    Added tests for text fields

--- src/field_tests.rs
+++ src/field_tests.rs
@@ -5,28 +5,25 @@ mod test {
     use field::*;
     use ser::Serializer;
     use de::Deserializer;
+    use std::vec::Vec;
+
+    fn text_to_vec(val: &str) -> Vec<u8> {
+        let mut bytes = vec![ val.len() as u8 ];
+
+        for byte in val.bytes() {
+            bytes.push(byte);
+        }
+
+        bytes
+    }
 
     #[test]
     fn text_serialize() {
         let mut ser = Serializer{ output: vec![] };
         let val = "test string";
+        let bytes = text_to_vec(val);
 
-        text::serialize(val, &mut ser);
-
-        let bytes = [
-            11,
-            't' as u8,
-            'e' as u8,
-            's' as u8,
-            't' as u8,
-            ' ' as u8,
-            's' as u8,
-            't' as u8,
-            'r' as u8,
-            'i' as u8,
-            'n' as u8,
-            'g' as u8
-        ];
+        text::serialize(val, &mut ser).unwrap();
 
         for i in 0..bytes.len() {
             println!("{}", i);
@@ -36,21 +33,12 @@ mod test {
 
     #[test]
     fn text_deserialize() {
-        let bytes = [
-            8,
-            'h' as u8,
-            'i' as u8,
-            ' ' as u8,
-            't' as u8,
-            'h' as u8,
-            'e' as u8,
-            'r' as u8,
-            'e' as u8
-        ];
+        let text = "hi there";
+        let bytes = text_to_vec(text);
 
-        let mut de = Deserializer{ bytes: &bytes };
+        let mut de = Deserializer{ bytes: &bytes[..] };
         let val = text::deserialize(&mut de).unwrap();
 
-        assert_eq!("hi there", &val);
+        assert_eq!(text, &val);
     }
 }
