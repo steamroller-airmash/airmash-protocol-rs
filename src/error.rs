ec4757217f041adf03035e970e29e5a8e98ee2c4
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-10-27 17:08:58 -0400

    Document error types
    EntityIdOutOfRangeError isn't used within the crate, this
    commit hides it from the docs and deprecates it.

--- src/error.rs
+++ src/error.rs
@@ -2,9 +2,16 @@
 
 use std::num::TryFromIntError;
 
-pub struct EntityIdOutOfRangeError;
+/// Attempted to convert an enum from a value but
+/// the value didn't map to any possible enum value.
 pub struct EnumValueOutOfRangeError<T>(pub T);
 
+// Can't get rid of this since it would be a breaking change.
+// TODO: When the major version is bumped, remove this.
+#[doc(hidden)]
+#[deprecated(note = "This isn't used anywhere in this crate")]
+pub struct EntityIdOutOfRangeError;
+#[allow(deprecated)]
 impl From<TryFromIntError> for EntityIdOutOfRangeError {
 	fn from(_: TryFromIntError) -> Self {
 		Self {}
