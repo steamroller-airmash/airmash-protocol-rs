3ce41399742f19ef8ffd31cacaaa14466a26f9c7
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-12-17 14:05:33 -0500

    Add missing upgrade type

--- src/enums/upgrade_type.rs
+++ src/enums/upgrade_type.rs
@@ -1,14 +1,23 @@
+
+#![allow(deprecated)]
+
 #[cfg(feature = "specs")]
 use specs::{Component, DenseVecStorage};
 
 impl_try_from_enum! {
-	/// TODO: Reverse engineer
-	///
-	/// This might be just [`PlaneType`][0] instead.
-	///
-	/// [0]: struct.PlaneType.html
+	/// All upgrade types.
 	#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 	#[cfg_attr(feature = "specs", derive(Component))]
 	#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-	pub enum UpgradeType {}
+	pub enum UpgradeType {
+		/// This seems to be sent by the official server when a
+		/// player leaves. Packets with this value are ignored
+		/// by the client, so they don't seem to affect gameplay
+		/// at all.
+		None = 0,
+		Speed = 1,
+		Defense = 2,
+		Energy = 3,
+		Missile = 4,
+	}
 }
