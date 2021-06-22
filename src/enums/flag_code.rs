15f4336c7547139443e1c271e605145c8aa8e401
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2021-06-22 11:53:16 -0400

    Move all enums into one module

--- src/enums/flag_code.rs
+++ src/enums/flag_code.rs
@@ -3,163 +3,163 @@ use specs::{Component, DenseVecStorage};
 use std::convert::TryFrom;
 use std::str::FromStr;
 
-/// All player flags currently available within
-/// the game.
-///
-/// This enum can be determined from a flag code
-/// string using the [`FromStr`][0] or
-/// [`TryFrom`][1] implementations. Usually the
-/// server will parse invalid flag code strings
-/// into the [`UnitedNations`][2] variant, but
-/// this is left up to the user.
-///
-/// # Restricted Flags
-/// In the official server the following flags are
-/// restricted to players level 4 and above:
-/// - [`JollyRogers`](#variant.JollyRogers)
-/// - [`Communist`](#variant.Communist)
-/// - [`ImperialJapan`](#variant.ImperialJapan)
-/// - [`Confederate`](#variant.Confederate)
-/// - [`Rainbow`](#variant.Rainbow)
-///
-/// Changing flags in-game are restricted to those
-/// level 2 and above, although any (non-restricted)
-/// flag can be chosen when logging in.
-///
-/// [0]: https://doc.rust-lang.org/std/str/trait.FromStr.html
-/// [1]: https://doc.rust-lang.org/std/convert/trait.TryFrom.html
-/// [2]: #variant.UnitedNations
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Primitive)]
-#[cfg_attr(feature = "specs", derive(Component))]
-pub enum FlagCode {
-	SyrianArabRepublic = 1,
-	Thailand = 2,
-	Turkmenistan = 3,
-	Tunisia = 4,
-	Turkey = 5,
-	TrinidadandTobago = 6,
-	Taiwan = 7,
-	Tanzania = 8,
-	Ukraine = 9,
-	UnitedNations = 10,
-	UnitedStates = 11,
-	Uruguay = 12,
-	Uzbekistan = 13,
-	Venezuela = 14,
-	VietNam = 15,
-	PuertoRico = 16,
-	Portugal = 17,
-	Paraguay = 18,
-	Qatar = 19,
-	Rainbow = 20,
-	Romania = 21,
-	Serbia = 22,
-	RussianFederation = 23,
-	SaudiArabia = 24,
-	Sweden = 25,
-	Singapore = 26,
-	Slovenia = 27,
-	Slovakia = 28,
-	SanMarino = 29,
-	Macedonia = 30,
-	Macao = 31,
-	Malta = 32,
-	Mexico = 33,
-	Malaysia = 34,
-	Nigeria = 35,
-	Netherlands = 36,
-	Norway = 37,
-	Nepal = 38,
-	NewZealand = 39,
-	Oman = 40,
-	Panama = 41,
-	Peru = 42,
-	Japan = 43,
-	DPRK = 44,
-	SouthKorea = 45,
-	Kuwait = 46,
-	Kazakhstan = 47,
-	Lebanon = 48,
-	Liechtenstein = 49,
-	SriLanka = 50,
-	Lithuania = 51,
-	Luxembourg = 52,
-	Latvia = 53,
-	Honduras = 54,
-	Croatia = 55,
-	Hungary = 56,
-	Indonesia = 57,
-	Ireland = 58,
-	Israel = 59,
-	IsleofMan = 60,
-	ImperialJapan = 61,
-	India = 62,
-	Iraq = 63,
-	Germany = 64,
-	Denmark = 65,
-	DominicanRepublic = 66,
-	Algeria = 67,
-	Ecuador = 68,
-	Estonia = 69,
-	Egypt = 70,
-	Spain = 71,
-	EuropeanUnion = 72,
-	Bahrain = 73,
-	Bolivia = 74,
-	Brazil = 75,
-	Bhutan = 76,
-	Belarus = 77,
-	Canada = 78,
-	Switzerland = 79,
-	Andorra = 80,
-	UnitedArabEmirates = 81,
-	Albania = 82,
-	Armenia = 83,
-	Chile = 84,
-	Antarctica = 85,
-	China = 86,
-	Argentina = 87,
-	Finland = 88,
-	Colombia = 89,
-	Austria = 90,
-	Iran = 91,
-	France = 92,
-	Communist = 93,
-	Australia = 94,
-	LibyanArabJamahiriya = 95,
-	Iceland = 96,
-	UnitedKingdom = 97,
-	Confederate = 98,
-	Azerbaijan = 99,
-	Morocco = 100,
-	Italy = 101,
-	Georgia = 102,
-	CostaRica = 103,
-	BosniaAndHerzegovina = 104,
-	Philippines = 105,
-	Monaco = 106,
-	Jamaica = 107,
-	Greece = 108,
-	Cuba = 109,
-	Bangladesh = 110,
-	Somalia = 111,
-	Pakistan = 112,
-	Moldova = 113,
-	Jordan = 114,
-	Guatemala = 115,
-	Cyprus = 116,
-	Belgium = 117,
-	SouthAfrica = 118,
-	ElSalvador = 119,
-	Poland = 120,
-	Montenegro = 121,
-	JollyRogers = 122,
-	HongKong = 123,
-	CzechRepublic = 124,
-	Bulgaria = 125,
+decl_enum! {
+	/// All player flags currently available within
+	/// the game.
+	///
+	/// This enum can be determined from a flag code
+	/// string using the [`FromStr`][0] or
+	/// [`TryFrom`][1] implementations. Usually the
+	/// server will parse invalid flag code strings
+	/// into the [`UnitedNations`][2] variant, but
+	/// this is left up to the user.
+	///
+	/// # Restricted Flags
+	/// In the official server the following flags are
+	/// restricted to players level 4 and above:
+	/// - [`JollyRogers`](#variant.JollyRogers)
+	/// - [`Communist`](#variant.Communist)
+	/// - [`ImperialJapan`](#variant.ImperialJapan)
+	/// - [`Confederate`](#variant.Confederate)
+	/// - [`Rainbow`](#variant.Rainbow)
+	///
+	/// Changing flags in-game are restricted to those
+	/// level 2 and above, although any (non-restricted)
+	/// flag can be chosen when logging in.
+	///
+	/// [0]: https://doc.rust-lang.org/std/str/trait.FromStr.html
+	/// [1]: https://doc.rust-lang.org/std/convert/trait.TryFrom.html
+	/// [2]: #variant.UnitedNations
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = UnitedNations]
+	pub enum FlagCode {
+		SyrianArabRepublic = 1,
+		Thailand = 2,
+		Turkmenistan = 3,
+		Tunisia = 4,
+		Turkey = 5,
+		TrinidadandTobago = 6,
+		Taiwan = 7,
+		Tanzania = 8,
+		Ukraine = 9,
+		UnitedNations = 10,
+		UnitedStates = 11,
+		Uruguay = 12,
+		Uzbekistan = 13,
+		Venezuela = 14,
+		VietNam = 15,
+		PuertoRico = 16,
+		Portugal = 17,
+		Paraguay = 18,
+		Qatar = 19,
+		Rainbow = 20,
+		Romania = 21,
+		Serbia = 22,
+		RussianFederation = 23,
+		SaudiArabia = 24,
+		Sweden = 25,
+		Singapore = 26,
+		Slovenia = 27,
+		Slovakia = 28,
+		SanMarino = 29,
+		Macedonia = 30,
+		Macao = 31,
+		Malta = 32,
+		Mexico = 33,
+		Malaysia = 34,
+		Nigeria = 35,
+		Netherlands = 36,
+		Norway = 37,
+		Nepal = 38,
+		NewZealand = 39,
+		Oman = 40,
+		Panama = 41,
+		Peru = 42,
+		Japan = 43,
+		DPRK = 44,
+		SouthKorea = 45,
+		Kuwait = 46,
+		Kazakhstan = 47,
+		Lebanon = 48,
+		Liechtenstein = 49,
+		SriLanka = 50,
+		Lithuania = 51,
+		Luxembourg = 52,
+		Latvia = 53,
+		Honduras = 54,
+		Croatia = 55,
+		Hungary = 56,
+		Indonesia = 57,
+		Ireland = 58,
+		Israel = 59,
+		IsleofMan = 60,
+		ImperialJapan = 61,
+		India = 62,
+		Iraq = 63,
+		Germany = 64,
+		Denmark = 65,
+		DominicanRepublic = 66,
+		Algeria = 67,
+		Ecuador = 68,
+		Estonia = 69,
+		Egypt = 70,
+		Spain = 71,
+		EuropeanUnion = 72,
+		Bahrain = 73,
+		Bolivia = 74,
+		Brazil = 75,
+		Bhutan = 76,
+		Belarus = 77,
+		Canada = 78,
+		Switzerland = 79,
+		Andorra = 80,
+		UnitedArabEmirates = 81,
+		Albania = 82,
+		Armenia = 83,
+		Chile = 84,
+		Antarctica = 85,
+		China = 86,
+		Argentina = 87,
+		Finland = 88,
+		Colombia = 89,
+		Austria = 90,
+		Iran = 91,
+		France = 92,
+		Communist = 93,
+		Australia = 94,
+		LibyanArabJamahiriya = 95,
+		Iceland = 96,
+		UnitedKingdom = 97,
+		Confederate = 98,
+		Azerbaijan = 99,
+		Morocco = 100,
+		Italy = 101,
+		Georgia = 102,
+		CostaRica = 103,
+		BosniaAndHerzegovina = 104,
+		Philippines = 105,
+		Monaco = 106,
+		Jamaica = 107,
+		Greece = 108,
+		Cuba = 109,
+		Bangladesh = 110,
+		Somalia = 111,
+		Pakistan = 112,
+		Moldova = 113,
+		Jordan = 114,
+		Guatemala = 115,
+		Cyprus = 116,
+		Belgium = 117,
+		SouthAfrica = 118,
+		ElSalvador = 119,
+		Poland = 120,
+		Montenegro = 121,
+		JollyRogers = 122,
+		HongKong = 123,
+		CzechRepublic = 124,
+		Bulgaria = 125,
+	}
 }
-
-impl_try_from2!(FlagCode);
 
 impl<'a> TryFrom<&'a str> for FlagCode {
 	type Error = <Self as TryFrom<String>>::Error;
@@ -188,12 +188,6 @@ impl FromStr for FlagCode {
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
 		Self::try_from(s)
-	}
-}
-
-impl Default for FlagCode {
-	fn default() -> Self {
-		FlagCode::UnitedNations
 	}
 }
 
--- src/enums/mod.rs
+++ src/enums/mod.rs
@@ -1,37 +1,267 @@
-mod command_reply_type;
-mod despawn_mob_type;
-mod error_type;
-mod firewall_status;
-mod firewall_update_type;
+#[macro_use]
+mod macros;
 mod flag_code;
-mod flag_update_type;
-mod game_type;
-mod key_code;
-mod leave_horizon_type;
-mod mob_type;
-mod plane_type;
-mod player_level_type;
-mod player_status;
-mod powerup_type;
-mod server_custom_type;
-mod server_message_type;
-mod upgrade_type;
 
-pub use self::command_reply_type::*;
-pub use self::despawn_mob_type::*;
-pub use self::error_type::*;
-pub use self::firewall_status::*;
-pub use self::firewall_update_type::*;
-pub use self::flag_code::*;
-pub use self::flag_update_type::*;
-pub use self::game_type::*;
-pub use self::key_code::*;
-pub use self::leave_horizon_type::*;
-pub use self::mob_type::*;
-pub use self::plane_type::*;
-pub use self::player_level_type::*;
-pub use self::player_status::*;
-pub use self::powerup_type::*;
-pub use self::server_custom_type::*;
-pub use self::server_message_type::*;
-pub use self::upgrade_type::*;
+pub use self::flag_code::FlagCode;
+
+#[cfg(feature = "specs")]
+use specs::{Component, DenseVecStorage};
+
+decl_enum! {
+	/// Specifies whether the debug reply to a command should
+	/// open a popup or be displayed in the chat window.
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = ShowInPopup]
+	pub enum CommandReplyType {
+		ShowInConsole = 0,
+		/// Technically this should be any value other than `0`,
+		/// the [`From`][0] integer implementation for this enum deals
+		/// with that.
+		///
+		/// [0]: https://doc.rust-lang.org/std/convert/trait.From.html
+		ShowInPopup = 1,
+	}
+
+  /// Details on how the mob despawned. (i.e. whether
+  /// it's lifetime ended or it collided with some
+  /// other object)
+  #[cfg_attr(feature = "specs", derive(Component))]
+  pub enum DespawnType {
+		LifetimeEnded = 0,
+		Collided = 1,
+  }
+
+	/// All error codes that can be sent to the client.
+	///
+	/// These are all server errors that the vanilla AIRMASH
+	/// client (and the current STARMASH client) understands.
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = UnknownError]
+	pub enum ErrorType {
+		DisconnectedForPacketFlooding = 1,
+		BannedForPacketFlooding = 2,
+		Banned = 3,
+		IdleRequiredBeforeRespawn = 5,
+		AfkTimeout = 6,
+		Kicked = 7,
+		InvalidLogin = 8,
+		IncorrectProtocolLevel = 9,
+		AccountBanned = 10,
+		AccountAlreadyLoggedIn = 11,
+		NoRespawnInBTR = 12,
+		IdleRequiredBeforeSpectate = 13,
+		NotEnoughUpgrades = 20,
+		ChatThrottled = 30,
+		FlagChangeThrottled = 31,
+		UnknownCommand = 100,
+
+		UnknownError = 255,
+	}
+
+	/// TODO: Reverse engineer
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = _Unknown]
+	pub enum FirewallStatus {
+		#[doc(hidden)]
+		/// Not a real value, just makes derives work
+		/// remove this once the enum is reverse engineered
+		_Unknown = 0,
+	}
+
+	/// TODO: Reverse engineer
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = _Unknown]
+	pub enum FirewallUpdateType {
+		#[doc(hidden)]
+		/// Not a real value, just makes derives work
+		/// remove this once the enum is reverse engineered
+		_Unknown = 0,
+	}
+
+	/// Flag update type
+	///
+	/// Used to indicate whether the flag is now being
+	/// carried by a player or whether the update
+	/// sets the position of the flag directly.
+	///
+	/// Used in:
+	/// - TODO
+	///
+	/// Implementors Note: This had a `TODO: rev-eng`
+	/// comment on it but it doesn't seem to be missing
+	/// any values.
+	#[cfg_attr(feature = "specs", derive(Component))]
+	pub enum FlagUpdateType {
+		Position = 1,
+		Carrier = 2,
+	}
+
+	/// Game Type.
+	///
+	/// Hopefully self explanatory, used to indicate to
+	/// the client which game is being played. The client
+	/// uses this to decide on player colouring and
+	/// whether or not to show the flags in-game.
+	/// It will also correspond with the type of detailed
+	/// score ([`ScoreDetailedFFA`][0], [`ScoreDetailedCTF`][1],
+	/// or [`ScoreDetailedBTR`][2]) that the client expects
+	/// to receive.
+	///
+	/// Used in:
+	/// - TODO
+	///
+	/// [0]: server/struct.ScoreDetailedFFA.html
+	/// [1]: server/struct.ScoreDetailedCTF.html
+	/// [2]: server/struct.ScoreDetailedBTR.html
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = FFA]
+	pub enum GameType {
+		FFA = 1,
+		CTF = 2,
+		BTR = 3,
+	}
+
+	/// The key that's had it's state changed.
+	/// This is only used for client -> server
+	/// communication.
+	///
+	/// It is used in the following packets:
+	/// - TODO
+	#[cfg_attr(feature = "specs", derive(Component))]
+	pub enum KeyCode {
+		Up = 1,
+		Down = 2,
+		Left = 3,
+		Right = 4,
+		Fire = 5,
+		Special = 6,
+	}
+
+	/// Indicates the type of entity that just
+	/// went outside of the player's horizon.
+	///
+	/// TODO: Complete reverse engineering this.
+	/// NOTE: The values here aren't in any way
+	/// certain and should be verified before
+	/// relying upon them.
+	#[cfg_attr(feature = "specs", derive(Component))]
+	pub enum LeaveHorizonType {
+		Player = 0,
+		Mob = 1,
+	}
+
+	/// Types of all mobs present in the game.
+	///
+	/// In AIRMASH, mobs are any non-player and non-wall
+	/// items that can be interacted with. This includes
+	/// powerups, upgrades, and all missiles.
+	///
+	/// Used by:
+	/// - TODO
+	#[cfg_attr(feature = "specs", derive(Component))]
+	pub enum MobType {
+		PredatorMissile = 1,
+		GoliathMissile = 2,
+		MohawkMissile = 3,
+		Upgrade = 4,
+		TornadoSingleMissile = 5,
+		TornadoTripleMissile = 6,
+		ProwlerMissile = 7,
+		Shield = 8,
+		Inferno = 9,
+	}
+
+	/// Used to indicate the type of plane
+	/// that the packet refers to.
+	///
+	/// Used in:
+	/// - TODO
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = Predator]
+	pub enum PlaneType {
+		Predator = 1,
+		Goliath = 2,
+		Mohawk = 3,
+		Tornado = 4,
+		Prowler = 5,
+	}
+
+	/// Indicate whether a player levelled up, or has
+	/// just logged in and their level is being communicated
+	/// to the client.
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = Login]
+	pub enum PlayerLevelType {
+		Login = 0,
+		LevelUp = 1,
+	}
+
+	/// Flag for indicating whether a player is
+	/// alive or dead.
+	///
+	/// This is used in the following packets:
+	/// - [`Login`][0] (specifically [`LoginPlayer`][1])
+	/// - [`PlayerNew`][2]
+	///
+	/// [0]: server/struct.login.html
+	/// [1]: server/struct.loginplayer.html
+	/// [2]: server/struct.playernew.html
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = Alive]
+	pub enum PlayerStatus {
+		Alive = 0,
+		Dead = 1,
+	}
+
+	/// TODO: Reverse engineer
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = Shield]
+	pub enum PowerupType {
+		Shield = 1,
+		/// This is just a guess.
+		/// TODO: Verify
+		Inferno = 2,
+	}
+
+	/// Specific identifiers for server custom messages.
+	///
+	/// TODO: Reverse Engineer
+	#[cfg_attr(feature = "specs", derive(Component))]
+	pub enum ServerCustomType {
+		/// TODO: Determine if this name is accurate
+		BTRWin = 1,
+		/// TODO: Determine if this name is accurate
+		CTFWin = 2,
+	}
+
+	/// Type specifier for server banner messages.
+	///
+	/// TODO: Reverse engineer
+	#[cfg_attr(feature = "specs", derive(Component))]
+	pub enum ServerMessageType {
+		TimeToGameStart = 1,
+		/// TODO: Verify the value of this one
+		Flag = 2,
+		/// New Type, used by this server for shutdown message
+		/// (once they work)
+		Shutdown = 15,
+		/// New Type, used by this server for banner messages
+		/// on player join.
+		Banner = 16,
+	}
+
+	/// All upgrade types.
+	#[cfg_attr(feature = "specs", derive(Component))]
+	##[default = None]
+	pub enum UpgradeType {
+		/// This seems to be sent by the official server when a
+		/// player leaves. Packets with this value are ignored
+		/// by the client, so they don't seem to affect gameplay
+		/// at all.
+		None = 0,
+		Speed = 1,
+		Defense = 2,
+		Energy = 3,
+		Missile = 4,
+	}
+}
