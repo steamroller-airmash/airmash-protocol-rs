14c2c37ca978eda7926f1c344001f2149e52758a
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2021-06-22 15:25:45 -0400

    Add result typedef to v5 module

--- src/v5/mod.rs
+++ src/v5/mod.rs
@@ -3,5 +3,7 @@
 mod error;
 mod protocol;
 
+type Result<T = ()> = std::result::Result<T, Error>;
+
 pub use self::error::{Error, ErrorExt, ErrorKind};
 pub use self::protocol::{AirmashDeserializerV5, AirmashSerializerV5, DeserializeV5, SerializeV5};
--- src/v5/protocol.rs
+++ src/v5/protocol.rs
@@ -1,8 +1,7 @@
 use crate::v5::{Error, ErrorExt as _, ErrorKind};
 use crate::Vector2;
 use bstr::{BStr, ByteSlice};
-
-type SResult<T = ()> = Result<T, Error>;
+use super::Result;
 
 struct ScalarSpec {
   shift: i32,
@@ -14,17 +13,17 @@ impl ScalarSpec {
     Self { shift, mult }
   }
 
-  fn de(&self, de: &mut AirmashDeserializerV5) -> SResult<f32> {
+  fn de(&self, de: &mut AirmashDeserializerV5) -> Result<f32> {
     Ok((((de.deserialize_u16()? as i32) - self.shift) as f32) * (1.0 / self.mult))
   }
-  fn ser(&self, ser: &mut AirmashSerializerV5, value: f32) -> SResult {
+  fn ser(&self, ser: &mut AirmashSerializerV5, value: f32) -> Result {
     ser.serialize_u16(((value * self.mult) as i32 + self.shift).clamp(0, u16::MAX as i32) as u16)
   }
 
-  fn de_u8(&self, de: &mut AirmashDeserializerV5) -> SResult<f32> {
+  fn de_u8(&self, de: &mut AirmashDeserializerV5) -> Result<f32> {
     Ok((((de.deserialize_u8()? as i32) - self.shift) as f32) * (1.0 / self.mult))
   }
-  fn ser_u8(&self, ser: &mut AirmashSerializerV5, value: f32) -> SResult {
+  fn ser_u8(&self, ser: &mut AirmashSerializerV5, value: f32) -> Result {
     ser.serialize_u8(((value * self.mult) as i32 + self.shift).clamp(0, u8::MAX as i32) as u8)
   }
 }
@@ -47,59 +46,59 @@ impl<'ser> AirmashSerializerV5<'ser> {
     Self { data }
   }
 
-  pub fn serialize<T: SerializeV5>(&mut self, value: &T) -> SResult {
+  pub fn serialize<T: SerializeV5>(&mut self, value: &T) -> Result {
     value.serialize(self)
   }
 
-  pub fn serialize_bytes(&mut self, bytes: &[u8]) -> SResult {
+  pub fn serialize_bytes(&mut self, bytes: &[u8]) -> Result {
     self.data.extend_from_slice(bytes);
     Ok(())
   }
 
-  pub fn serialize_u8(&mut self, value: u8) -> SResult {
+  pub fn serialize_u8(&mut self, value: u8) -> Result {
     self.serialize_bytes(&[value])
   }
-  pub fn serialize_u16(&mut self, value: u16) -> SResult {
+  pub fn serialize_u16(&mut self, value: u16) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
-  pub fn serialize_u32(&mut self, value: u32) -> SResult {
+  pub fn serialize_u32(&mut self, value: u32) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
-  pub fn serialize_u64(&mut self, value: u64) -> SResult {
+  pub fn serialize_u64(&mut self, value: u64) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
-  pub fn serialize_u128(&mut self, value: u128) -> SResult {
+  pub fn serialize_u128(&mut self, value: u128) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
 
-  pub fn serialize_i8(&mut self, value: i8) -> SResult {
+  pub fn serialize_i8(&mut self, value: i8) -> Result {
     self.serialize_u8(value as u8)
   }
-  pub fn serialize_i16(&mut self, value: i16) -> SResult {
+  pub fn serialize_i16(&mut self, value: i16) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
-  pub fn serialize_i32(&mut self, value: i32) -> SResult {
+  pub fn serialize_i32(&mut self, value: i32) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
-  pub fn serialize_i64(&mut self, value: i64) -> SResult {
+  pub fn serialize_i64(&mut self, value: i64) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
-  pub fn serialize_i128(&mut self, value: i128) -> SResult {
-    self.serialize_bytes(&value.to_le_bytes())
-  }
-
-  pub fn serialize_f32(&mut self, value: f32) -> SResult {
-    self.serialize_bytes(&value.to_le_bytes())
-  }
-  pub fn serialize_f64(&mut self, value: f64) -> SResult {
+  pub fn serialize_i128(&mut self, value: i128) -> Result {
     self.serialize_bytes(&value.to_le_bytes())
   }
 
-  pub fn serialize_u24(&mut self, value: u32) -> SResult {
+  pub fn serialize_f32(&mut self, value: f32) -> Result {
+    self.serialize_bytes(&value.to_le_bytes())
+  }
+  pub fn serialize_f64(&mut self, value: f64) -> Result {
+    self.serialize_bytes(&value.to_le_bytes())
+  }
+
+  pub fn serialize_u24(&mut self, value: u32) -> Result {
     self.serialize_bytes(&value.to_le_bytes()[..3])
   }
 
-  pub fn serialize_array_small<T>(&mut self, data: &[T]) -> SResult
+  pub fn serialize_array_small<T>(&mut self, data: &[T]) -> Result
   where
     T: SerializeV5,
   {
@@ -115,7 +114,7 @@ impl<'ser> AirmashSerializerV5<'ser> {
 
     Ok(())
   }
-  pub fn serialize_array_large<T>(&mut self, data: &[T]) -> SResult
+  pub fn serialize_array_large<T>(&mut self, data: &[T]) -> Result
   where
     T: SerializeV5,
   {
@@ -132,7 +131,7 @@ impl<'ser> AirmashSerializerV5<'ser> {
     Ok(())
   }
 
-  pub fn serialize_text_small<T>(&mut self, data: &BStr) -> SResult {
+  pub fn serialize_text_small<T>(&mut self, data: &BStr) -> Result {
     if data.len() > u8::MAX as usize {
       return Err(Error::new(ErrorKind::ArraySizeTooLarge));
     }
@@ -140,7 +139,7 @@ impl<'ser> AirmashSerializerV5<'ser> {
     self.serialize_u8(data.len() as u8)?;
     self.serialize_bytes(data.as_bytes())
   }
-  pub fn serialize_text_large<T>(&mut self, data: &BStr) -> SResult {
+  pub fn serialize_text_large<T>(&mut self, data: &BStr) -> Result {
     if data.len() > u16::MAX as usize {
       return Err(Error::new(ErrorKind::ArraySizeTooLarge));
     }
@@ -149,11 +148,11 @@ impl<'ser> AirmashSerializerV5<'ser> {
     self.serialize_bytes(data.as_bytes())
   }
 
-  pub fn serialize_accel(&mut self, v: Vector2<f32>) -> SResult {
+  pub fn serialize_accel(&mut self, v: Vector2<f32>) -> Result {
     ACCEL_SPEC.ser(self, v[0])?;
     ACCEL_SPEC.ser(self, v[1])
   }
-  pub fn serialize_low_res_pos(&mut self, pos: Option<Vector2<f32>>) -> SResult {
+  pub fn serialize_low_res_pos(&mut self, pos: Option<Vector2<f32>>) -> Result {
     let x = pos
       .map(|v| ((v[0] / 128.0) as i32 + 128) as u8)
       .unwrap_or(0);
@@ -164,44 +163,44 @@ impl<'ser> AirmashSerializerV5<'ser> {
     x.serialize(self)?;
     y.serialize(self)
   }
-  pub fn serialize_pos_f32(&mut self, pos: Vector2<f32>) -> SResult {
+  pub fn serialize_pos_f32(&mut self, pos: Vector2<f32>) -> Result {
     self.serialize_f32(pos[0])?;
     self.serialize_f32(pos[1])
   }
-  pub fn serialize_pos(&mut self, pos: Vector2<f32>) -> SResult {
+  pub fn serialize_pos(&mut self, pos: Vector2<f32>) -> Result {
     self.serialize_coordx(pos[0])?;
     self.serialize_coordy(pos[1])
   }
-  pub fn serialize_pos24(&mut self, pos: Vector2<f32>) -> SResult {
+  pub fn serialize_pos24(&mut self, pos: Vector2<f32>) -> Result {
     self.serialize_coord24(pos[0])?;
     self.serialize_coord24(pos[1])
   }
-  pub fn serialize_vel(&mut self, pos: Vector2<f32>) -> SResult {
+  pub fn serialize_vel(&mut self, pos: Vector2<f32>) -> Result {
     self.serialize_speed(pos[0])?;
     self.serialize_speed(pos[1])
   }
 
-  pub fn serialize_coord24(&mut self, v: f32) -> SResult {
+  pub fn serialize_coord24(&mut self, v: f32) -> Result {
     let spec = COORD24_SPEC;
     self
       .serialize_u24(((v * spec.mult) as i32 + spec.shift).clamp(0, (u32::MAX >> 8) as i32) as u32)
   }
-  pub fn serialize_coordx(&mut self, v: f32) -> SResult {
+  pub fn serialize_coordx(&mut self, v: f32) -> Result {
     COORDX_SPEC.ser(self, v)
   }
-  pub fn serialize_coordy(&mut self, v: f32) -> SResult {
+  pub fn serialize_coordy(&mut self, v: f32) -> Result {
     COORDY_SPEC.ser(self, v)
   }
-  pub fn serialize_energy(&mut self, v: f32) -> SResult {
+  pub fn serialize_energy(&mut self, v: f32) -> Result {
     ENERGY_SPEC.ser_u8(self, v)
   }
-  pub fn serialize_regen(&mut self, v: f32) -> SResult {
+  pub fn serialize_regen(&mut self, v: f32) -> Result {
     REGEN_SPEC.ser(self, v)
   }
-  pub fn serialize_rotation(&mut self, v: f32) -> SResult {
+  pub fn serialize_rotation(&mut self, v: f32) -> Result {
     ROTATION_SPEC.ser(self, v)
   }
-  pub fn serialize_speed(&mut self, v: f32) -> SResult {
+  pub fn serialize_speed(&mut self, v: f32) -> Result {
     SPEED_SPEC.ser(self, v)
   }
 }
@@ -215,11 +214,11 @@ impl<'de> AirmashDeserializerV5<'de> {
     Self { data }
   }
 
-  pub fn deserialize<T: DeserializeV5<'de>>(&mut self) -> SResult<T> {
+  pub fn deserialize<T: DeserializeV5<'de>>(&mut self) -> Result<T> {
     T::deserialize(self)
   }
 
-  pub fn deserialize_fixed<const N: usize>(&mut self) -> SResult<[u8; N]> {
+  pub fn deserialize_fixed<const N: usize>(&mut self) -> Result<[u8; N]> {
     use std::convert::TryInto;
 
     let slice: [u8; N] = self.data[..N]
@@ -228,7 +227,7 @@ impl<'de> AirmashDeserializerV5<'de> {
     self.data = &self.data[N..];
     Ok(slice)
   }
-  pub fn deserialize_bytes(&mut self, len: usize) -> SResult<&'de [u8]> {
+  pub fn deserialize_bytes(&mut self, len: usize) -> Result<&'de [u8]> {
     if self.data.len() < len {
       return Err(Error::new(ErrorKind::EndOfBuffer));
     }
@@ -238,7 +237,7 @@ impl<'de> AirmashDeserializerV5<'de> {
     Ok(slice)
   }
 
-  pub fn deserialize_u8(&mut self) -> SResult<u8> {
+  pub fn deserialize_u8(&mut self) -> Result<u8> {
     if self.data.is_empty() {
       return Err(Error::new(ErrorKind::EndOfBuffer));
     }
@@ -247,48 +246,48 @@ impl<'de> AirmashDeserializerV5<'de> {
     self.data = &self.data[1..];
     Ok(value)
   }
-  pub fn deserialize_u16(&mut self) -> SResult<u16> {
+  pub fn deserialize_u16(&mut self) -> Result<u16> {
     self.deserialize_fixed().map(u16::from_le_bytes)
   }
-  pub fn deserialize_u32(&mut self) -> SResult<u32> {
+  pub fn deserialize_u32(&mut self) -> Result<u32> {
     self.deserialize_fixed().map(u32::from_le_bytes)
   }
-  pub fn deserialize_u64(&mut self) -> SResult<u64> {
+  pub fn deserialize_u64(&mut self) -> Result<u64> {
     self.deserialize_fixed().map(u64::from_le_bytes)
   }
-  pub fn deserialize_u128(&mut self) -> SResult<u128> {
+  pub fn deserialize_u128(&mut self) -> Result<u128> {
     self.deserialize_fixed().map(u128::from_le_bytes)
   }
 
-  pub fn deserialize_i8(&mut self) -> SResult<i8> {
+  pub fn deserialize_i8(&mut self) -> Result<i8> {
     self.deserialize_u8().map(|x| x as i8)
   }
-  pub fn deserialize_i16(&mut self) -> SResult<i16> {
+  pub fn deserialize_i16(&mut self) -> Result<i16> {
     self.deserialize_fixed().map(i16::from_le_bytes)
   }
-  pub fn deserialize_i32(&mut self) -> SResult<i32> {
+  pub fn deserialize_i32(&mut self) -> Result<i32> {
     self.deserialize_fixed().map(i32::from_le_bytes)
   }
-  pub fn deserialize_i64(&mut self) -> SResult<i64> {
+  pub fn deserialize_i64(&mut self) -> Result<i64> {
     self.deserialize_fixed().map(i64::from_le_bytes)
   }
-  pub fn deserialize_i128(&mut self) -> SResult<i128> {
+  pub fn deserialize_i128(&mut self) -> Result<i128> {
     self.deserialize_fixed().map(i128::from_le_bytes)
   }
 
-  pub fn deserialize_f32(&mut self) -> SResult<f32> {
+  pub fn deserialize_f32(&mut self) -> Result<f32> {
     self.deserialize_fixed().map(f32::from_le_bytes)
   }
-  pub fn deserialize_f64(&mut self) -> SResult<f64> {
+  pub fn deserialize_f64(&mut self) -> Result<f64> {
     self.deserialize_fixed().map(f64::from_be_bytes)
   }
 
-  pub fn deserialize_u24(&mut self) -> SResult<u32> {
+  pub fn deserialize_u24(&mut self) -> Result<u32> {
     let [a, b, c] = self.deserialize_fixed()?;
     Ok(u32::from_le_bytes([a, b, c, 0]))
   }
 
-  pub fn deserialize_array_small<T>(&mut self) -> SResult<Vec<T>>
+  pub fn deserialize_array_small<T>(&mut self) -> Result<Vec<T>>
   where
     T: DeserializeV5<'de>,
   {
@@ -301,7 +300,7 @@ impl<'de> AirmashDeserializerV5<'de> {
 
     Ok(data)
   }
-  pub fn deserialize_array_large<T>(&mut self) -> SResult<Vec<T>>
+  pub fn deserialize_array_large<T>(&mut self) -> Result<Vec<T>>
   where
     T: DeserializeV5<'de>,
   {
@@ -315,14 +314,14 @@ impl<'de> AirmashDeserializerV5<'de> {
     Ok(data)
   }
 
-  pub fn deserialize_text_small<T>(&mut self) -> SResult<&'de BStr>
+  pub fn deserialize_text_small<T>(&mut self) -> Result<&'de BStr>
   where
     T: DeserializeV5<'de>,
   {
     let len = self.deserialize_u8()? as usize;
     Ok(self.deserialize_bytes(len)?.into())
   }
-  pub fn deserialize_text_large<T>(&mut self) -> SResult<&'de BStr>
+  pub fn deserialize_text_large<T>(&mut self) -> Result<&'de BStr>
   where
     T: DeserializeV5<'de>,
   {
@@ -330,10 +329,10 @@ impl<'de> AirmashDeserializerV5<'de> {
     Ok(self.deserialize_bytes(len)?.into())
   }
 
-  pub fn deserialize_accel(&mut self) -> SResult<Vector2<f32>> {
+  pub fn deserialize_accel(&mut self) -> Result<Vector2<f32>> {
     Ok(Vector2::new(ACCEL_SPEC.de(self)?, ACCEL_SPEC.de(self)?))
   }
-  pub fn deserialize_low_res_pos(&mut self) -> SResult<Option<Vector2<f32>>> {
+  pub fn deserialize_low_res_pos(&mut self) -> Result<Option<Vector2<f32>>> {
     let (x, y): (u8, u8) = self.deserialize()?;
 
     if x == 0 && y == 0 {
@@ -345,73 +344,73 @@ impl<'de> AirmashDeserializerV5<'de> {
       ((y as i32 - 128) * 128) as f32,
     )))
   }
-  pub fn deserialize_pos_f32(&mut self) -> SResult<Vector2<f32>> {
+  pub fn deserialize_pos_f32(&mut self) -> Result<Vector2<f32>> {
     Ok(Vector2::new(
       self.deserialize_f32()?,
       self.deserialize_f32()?,
     ))
   }
-  pub fn deserialize_pos(&mut self) -> SResult<Vector2<f32>> {
+  pub fn deserialize_pos(&mut self) -> Result<Vector2<f32>> {
     Ok(Vector2::new(
       self.deserialize_coordx()?,
       self.deserialize_coordy()?,
     ))
   }
-  pub fn deserialize_pos24(&mut self) -> SResult<Vector2<f32>> {
+  pub fn deserialize_pos24(&mut self) -> Result<Vector2<f32>> {
     Ok(Vector2::new(
       self.deserialize_coord24()?,
       self.deserialize_coord24()?,
     ))
   }
-  pub fn deserialize_vel(&mut self) -> SResult<Vector2<f32>> {
+  pub fn deserialize_vel(&mut self) -> Result<Vector2<f32>> {
     Ok(Vector2::new(
       self.deserialize_speed()?,
       self.deserialize_speed()?,
     ))
   }
 
-  pub fn deserialize_coord24(&mut self) -> SResult<f32> {
+  pub fn deserialize_coord24(&mut self) -> Result<f32> {
     let spec = COORD24_SPEC;
     Ok((((self.deserialize_u24()? as i32) - spec.shift) as f32) * (1.0 / spec.mult))
   }
-  pub fn deserialize_coordx(&mut self) -> SResult<f32> {
+  pub fn deserialize_coordx(&mut self) -> Result<f32> {
     COORDX_SPEC.de(self)
   }
-  pub fn deserialize_coordy(&mut self) -> SResult<f32> {
+  pub fn deserialize_coordy(&mut self) -> Result<f32> {
     COORDY_SPEC.de(self)
   }
-  pub fn deserialize_energy(&mut self) -> SResult<f32> {
+  pub fn deserialize_energy(&mut self) -> Result<f32> {
     ENERGY_SPEC.de_u8(self)
   }
-  pub fn deserialize_regen(&mut self) -> SResult<f32> {
+  pub fn deserialize_regen(&mut self) -> Result<f32> {
     REGEN_SPEC.de(self)
   }
-  pub fn deserialize_rotation(&mut self) -> SResult<f32> {
+  pub fn deserialize_rotation(&mut self) -> Result<f32> {
     ROTATION_SPEC.de(self)
   }
-  pub fn deserialize_speed(&mut self) -> SResult<f32> {
+  pub fn deserialize_speed(&mut self) -> Result<f32> {
     SPEED_SPEC.de(self)
   }
 }
 
 pub trait SerializeV5 {
-  fn serialize<'ser>(&self, ser: &mut AirmashSerializerV5<'ser>) -> SResult;
+  fn serialize<'ser>(&self, ser: &mut AirmashSerializerV5<'ser>) -> Result;
 }
 
 pub trait DeserializeV5<'de>: Sized {
-  fn deserialize(de: &mut AirmashDeserializerV5<'de>) -> SResult<Self>;
+  fn deserialize(de: &mut AirmashDeserializerV5<'de>) -> Result<Self>;
 }
 
 macro_rules! impl_builtin {
   ($ty:ty, $ser:ident, $de:ident) => {
     impl SerializeV5 for $ty {
-      fn serialize<'ser>(&self, ser: &mut AirmashSerializerV5<'ser>) -> SResult {
+      fn serialize<'ser>(&self, ser: &mut AirmashSerializerV5<'ser>) -> Result {
         ser.$ser(*self)
       }
     }
 
     impl<'de> DeserializeV5<'de> for $ty {
-      fn deserialize(de: &mut AirmashDeserializerV5<'de>) -> SResult<Self> {
+      fn deserialize(de: &mut AirmashDeserializerV5<'de>) -> Result<Self> {
         de.$de()
       }
     }
@@ -429,7 +428,7 @@ macro_rules! impl_tuple {
     where $( $name: SerializeV5 ),*
     {
       #[allow(unused_variables, non_snake_case)]
-      fn serialize<'ser>(&self, ser: &mut AirmashSerializerV5<'ser>) -> SResult {
+      fn serialize<'ser>(&self, ser: &mut AirmashSerializerV5<'ser>) -> Result {
 				let ($( $name, )*) = self;
 
         $( $name.serialize(ser)?; )*
@@ -442,7 +441,7 @@ macro_rules! impl_tuple {
     where $( $name: DeserializeV5<'de> ),*
     {
       #[allow(unused_variables, non_snake_case)]
-      fn deserialize(de: &mut AirmashDeserializerV5<'de>) -> SResult<Self> {
+      fn deserialize(de: &mut AirmashDeserializerV5<'de>) -> Result<Self> {
         Ok(( $( <$name>::deserialize(de)?, )* ))
 			}
     }
