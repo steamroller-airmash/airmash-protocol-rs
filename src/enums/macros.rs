b144a0da7f7fa05d63abadbd2659c17ff32b16a5
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-24 00:44:33 -0400

    Add an unknown variant to all protocol enums (#19)
    * Add an unknown variant to all protocol enums
    * Fix some more compilation errors
    * Add `#[non_exhaustive]` to the last few enums

--- src/enums/macros.rs
+++ src/enums/macros.rs
@@ -1,58 +1,3 @@
-macro_rules! decl_enum_from_to {
-  {
-    $( default = $default:ident; )?
-    enum $name:ident;
-  } => {
-    decl_enum_from_to!($(default = $default, )? $name, u8,   to_u8,   from_u8);
-    decl_enum_from_to!($(default = $default, )? $name, u16,  to_u16,  from_u16);
-    decl_enum_from_to!($(default = $default, )? $name, u32,  to_u32,  from_u32);
-    decl_enum_from_to!($(default = $default, )? $name, u64,  to_u64,  from_u64);
-    decl_enum_from_to!($(default = $default, )? $name, u128, to_u128, from_u128);
-
-    decl_enum_from_to!($(default = $default, )? $name, i8,   to_i8,   from_i8);
-    decl_enum_from_to!($(default = $default, )? $name, i16,  to_i16,  from_i16);
-    decl_enum_from_to!($(default = $default, )? $name, i32,  to_i32,  from_i32);
-    decl_enum_from_to!($(default = $default, )? $name, i64,  to_i64,  from_i64);
-    decl_enum_from_to!($(default = $default, )? $name, i128, to_i128, from_i128);
-  };
-
-  (default = $default:ident, $name:ident, $ty:ident, $to:ident, $from:ident) => {
-    impl ::std::convert::From<$ty> for $name {
-      fn from(v: $ty) -> Self {
-        use ::num_traits::FromPrimitive;
-        Self::$from(v).unwrap_or(Self::default())
-      }
-    }
-
-    impl ::std::convert::From<$name> for $ty {
-      fn from(v: $name) -> Self {
-        use ::num_traits::ToPrimitive;
-        v.$to().expect("Type too large for primitive")
-      }
-    }
-  };
-  ($name:ident, $ty:ident, $to:ident, $from:ident) => {
-    impl ::std::convert::TryFrom<$ty> for $name {
-      type Error = crate::EnumValueOutOfRangeError<$ty>;
-
-      fn try_from(v: $ty) -> Result<Self, Self::Error> {
-        use ::num_traits::FromPrimitive;
-        match Self::$from(v) {
-          Some(x) => Ok(x),
-          None => Err(crate::EnumValueOutOfRangeError(v))
-        }
-      }
-    }
-
-    impl ::std::convert::From<$name> for $ty {
-      fn from(v: $name) -> Self {
-        use ::num_traits::ToPrimitive;
-        v.$to().expect("Type too large for primitive")
-      }
-    }
-  }
-}
-
 macro_rules! decl_serde_v5 {
   {
     enum $name:ident ;
@@ -89,6 +34,15 @@ macro_rules! decl_serde_v5 {
   }
 }
 
+macro_rules! enum_basetype {
+  ($basety:ty) => {
+    $basety
+  };
+  () => {
+    u8
+  };
+}
+
 macro_rules! decl_enum {
   {
     $(
@@ -110,40 +64,33 @@ macro_rules! decl_enum {
       $vis enum $name {
         $(
           $( #[$elemattr] )*
-          $elem = $value,
+          $elem,
         )*
+
+        Unknown(enum_basetype!($($basety)?))
       }
 
-      impl ::num_traits::FromPrimitive for $name {
-        fn from_u64(val: u64) -> Option<Self> {
-          Some(match val as _ {
-            $( $value => Self::$elem, )*
-            _ => return None
-          })
+      const _: () = {
+        type BaseTy = enum_basetype!($($basety)?);
+
+        impl From<BaseTy> for $name {
+          fn from(v: BaseTy) -> Self {
+            match v {
+              $( $value => Self::$elem, )*
+              v => Self::Unknown(v)
+            }
+          }
         }
 
-        fn from_i64(val: i64) -> Option<Self> {
-          Some(match val as _ {
-            $( $value => Self::$elem, )*
-            _ => return None
-          })
+        impl From<$name> for BaseTy {
+          fn from(v: $name) -> Self {
+            match v {
+              $( $name::$elem => $value, )*
+              $name::Unknown(v) => v,
+            }
+          }
         }
-      }
-
-      impl ::num_traits::ToPrimitive for $name {
-        fn to_u64(&self) -> Option<u64> {
-          Some(*self as isize as u64)
-        }
-
-        fn to_i64(&self) -> Option<i64> {
-          Some(*self as isize as i64)
-        }
-      }
-
-      decl_enum_from_to!{
-        $( default = $default; )?
-        enum $name;
-      }
+      };
 
       $(
         impl ::std::default::Default for $name {
